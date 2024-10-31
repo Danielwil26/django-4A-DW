9a422b066c6bdd48e706dd81a402eb420c27b46a
danielwil26 <Daniel>
2024-10-31 08:31:02 +0100

    działa

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -1,21 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+from django.shortcuts import get_object_or_404,render
+from django.http import HttpResponse, Http404
+from .models import Question, Choice
 from django.template import loader
 from django.http import Http404
+import logging
+
+logger = logging.getLogger(__name__)
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5] 
     output = ", ".join([q.question_text for q in latest_question_list])
+    logger.info(output)
     template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(output)
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     try:
-        question = Question.objects.get(pk=question_id)
+        question = get_object_or_404(Question, pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, "polls/detail.html", {"question": question})
